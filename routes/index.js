6a09f2ed8574720dcf54f168420a3be73a3e1b1e
Wmugacha <wilfredmugacha@gmail.com>
2023-12-05 15:58:00 +0000

    routes & endpoints

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,10 @@
 import { Router } from 'express';
 import AppController from '../controllers/AppController';
-import AppController from '../controllers/UsersController';
 
 const router = Router();
 
 router.get('/status', AppController.getStatus);
 
-router.get('/stats', AppController.getStatus);
+router.get('/stats', AppController.getStats);
 
 module.exports = router;
